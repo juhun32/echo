bebfc9b1df48f6800eb6e6e125bf02d394ecc2b7
Juhun Park <riann3207@gmail.com>
2026-02-15 01:56:31 -0500

    feat: vercel analytics

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 	let { children } = $props();
 </script>
 
